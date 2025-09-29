6022dcdbc4a0bf55500b5408b5e2f2f54012eaaf
Pablo <jdommay863@g.educaand.es>
2025-09-29 18:45:26 +0200

    ultimos cambios creado clase actividad 10

---
+++ Ejercicios/ActividadesU2/src/com/Actividad10.java
@@ -0,0 +1,15 @@
+package com;
+
+public class Actividad10 {
+	
+	public static void main(String [] args) {
+		/**
+		 *###############################################
+		 *#                LITERALES                    #
+		 *############################################### 
+		 **/
+		final String CAPICUA = "capicúa";
+		
+	}
+
+}
