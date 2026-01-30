4a974300f103da45cb9ba347cda972971ef7709c
robertofrancobarrios <franco.barrios.roberto@iescamas.es>
2026-01-30 16:59:14 +0100

    Add files via upload

---
+++ tablas-caracteres/src/main/java/es/iescamas/programacion/App.java
@@ -0,0 +1,10 @@
+package es.iescamas.programacion;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
